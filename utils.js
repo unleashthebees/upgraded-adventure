532299952218ae95c0e2cc3b20e1421fc5608db8
Ben <heavpotbuster@googlemail.com>
2018-12-09 22:48:17 +0100

    very basic charsheet and formula parsing

--- utils.js
+++ utils.js
@@ -1,3 +1,20 @@
+function val(formula) {
+	try {
+		var result = eval(formula);
+		return result;
+	} catch (e) {
+		return formula;
+	}
+}
+
 function BAB_3_4(clvl) {
 	return Math.floor(clvl*3/4);
+}
+
+function SAVE_GOOD(clvl) {
+	return Math.floor(clvl*2/3);
+}
+
+function SAVE_POOR(clvl) {
+	return Math.floor(clvl/3);
 }
