3f968ad4080d123372cff13daf6d373a5a28d96e
Ben <heavpotbuster@googlemail.com>
2019-01-11 01:51:25 +0100

    validators

--- validators.js
+++ validators.js
@@ -1,18 +1,38 @@
 // TODO: validate these:
-	// abilities
+	// base attack bonus
+	// base save bonuses
 	// increases at levels
 	// feats
-	// traits
+	// traits (have to be of different groups)
 	// skills
 	// 		missing skills
 	// 		class skills
-	// hit dice (amount = clvl)
-	// other fields (e.g. race=human grants an extra feat and extra skill ranks)
+	//		skills per level (depends on class)
+	// granted fields (e.g. race=human grants an extra feat and extra skill ranks)
 	function showValidatorInfo(parent) {
 		let errorList = [];
-		if (!stats.classes) errorList.push(`No classes`);
-		if (!stats.race) errorList.push(`Missing race`);
-		
-		errorList.map(err => (parent.append(`<div>${err}</div>`)));
-		if (errorList.length > 0) parent.append("<hr>");
+		try {
+			if (!stats.classes) errorList.push("No classes");
+			if (!stats.race) errorList.push("No race");
+			if (!stats.STR) errorList.push("No STR score");
+			if (!stats.DEX) errorList.push("No DEX score");
+			if (!stats.CON) errorList.push("No CON score");
+			if (!stats.INT) errorList.push("No INT score");
+			if (!stats.WIS) errorList.push("No WIS score");
+			if (!stats.CHA) errorList.push("No CHA score");
+			if (!stats.HD) errorList.push("No Hit Dice");
+			if (!stats.clvl) errorList.push("No Total Character Level (clvl)");
+			if (stats.HD.length != stats.clvl) {
+				errorList.push(`|Hit Dice ${stats.HD}| &ne; clvl ${stats.clvl}`);
+			}
+			let traits = stats.innate.filter(x => x.name.match("\\(Trait\\)"));
+			if (traits.length != 3) {
+				errorList.push(`Exactly 3 traits are required. (Selected: ${
+					traits.map(x=>x.name).join(", ") || "None"})`);
+			}
+		} finally {
+
+			errorList.map(err => (parent.append(`<div>${err}</div>`)));
+			if (errorList.length > 0) parent.append("<hr>");
+		}
 	}
