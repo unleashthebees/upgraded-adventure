f175bafaf94760273a3b44c379ba358e06574aec
unleashthebees <benjamin.hintze@googlemail.com>
2019-02-02 16:57:24 +0100

    Update jaro.js

--- characters/jaro.js
+++ characters/jaro.js
@@ -125,12 +125,8 @@ characterSheet = {
 			info: "one level below max spell level"
 		},
 		{
-			name: "Alertness",
-			bonus: { 
-				data: [
-					"perception+2", "sense_motive+2"],
-				type: ""}, info: "+4 when 10 ranks"
-			       },
+			name: "Alertness"
+		},
 		{
 			name: "Class Skills (Witch)",
 			bonus: {
