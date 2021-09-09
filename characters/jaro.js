07ad958d486db8ff435f52d5d6f7c0f125b16536
Ben <heavpotbuster@googlemail.com>
2021-09-09 22:53:03 +0200

    jaro

--- characters/jaro.js
+++ characters/jaro.js
@@ -1,9 +1,9 @@
 characterSheet = {
 	name: "Jaro Imaradi",
 	race: "Human",
-	classes: "Witch 8",
-	HD: [6, 6, 5, 4, 6, 5, 1, 5],
-	clvl: 8,
+	classes: "Witch 9",
+	HD: [6, 6, 5, 4, 6, 5, 1, 5, 1],
+	clvl: 9,
 	STR: 8,
 	DEX: 14,
 	CON: 14,
@@ -16,26 +16,27 @@ characterSheet = {
 	WIL: "SAVE_GOOD(stats.clvl)",
 	skillsPerLevel: 2,
 	skillranks: {
-		bluff: 8,
+		bluff: 9,
 		craft_clothing: 1,
-		diplomacy: 8,
-		disable_device: 8,
-		disguise: 8,
-		escape_artist: 8,
+		diplomacy: 9,
+		disable_device: 9,
+		disguise: 9,
+		handle_animal: 9,
 		fly: 1,
 		heal: 1,
-		knowledge_arcana: 8,
-		knowledge_dungeoneering: 1,
+		knowledge_arcana: 9,
+		knowledge_dungeoneering: 9,
 		knowledge_engineering: 1,
 		knowledge_geography: 1,
 		knowledge_history: 1,
-		knowledge_local: 8,
+		knowledge_local: 9,
 		knowledge_nature: 1,
 		knowledge_nobility: 1,
 		knowledge_planes: 1,
 		knowledge_religion: 1,
-		perception: 8,
-		spellcraft: 8,
+		perception: 9,
+		sense_motive: 1,
+		spellcraft: 9,
 		use_magic_device: 1
 	},
 	items: [
@@ -58,20 +59,26 @@ characterSheet = {
 			price: 5200
 		},
 		{
-			name: "Headband of Vast Intelligence",
+			name: "Headband of Vast Intelligence +2 (not worn)",
 			slot: "headband",
-			bonus: {
-				data: ["INT+2"],
-				type: "enhancement"
-			},
 			ranks: "escape_artist",
 			price: 4000
+		},
+		{
+			name: "Headband of Vast Intelligence +4",
+			slot: "headband",
+			ranks: "handle_animal, knowledge_dungeoneering",
+			bonus: {
+				data: ["INT+4"],
+				type: "enhancement"
+			},
+			info: "overrides 1 rank knowledge_dungeoneering"
 		},
 		{
 			name: "Cloak of Resistance",
 			slot: "shoulders",
 			bonus: {
-				data: ["FOR+2", "REF+2", "WIL+2"],
+				data: ["FOR+1", "REF+1", "WIL+1"],
 				type: "resistance"
 			},
 			price: 1000
@@ -122,16 +129,33 @@ characterSheet = {
 			}
 		},
 		{
-			name: "Favored Class Bonus (Levels 1-8)",
+			name: "Languages",
+			info: "Elven, Gnome, Ork, Dwarven, Halfling"
+		},
+		{
+			name: "Favored Class Bonus (Levels 1-9)",
 			bonus: {
 				data: [
 					"SKILLRANKS+4",
 					"SPELLSKNOWN_WITCH_2+2",
-					"SPELLSKNOWN_WITCH_3+2"
+					"SPELLSKNOWN_WITCH_3+2",
+					"SPELLSKNOWN_WITCH_4+1"
 				],
 				type: ""
 			},
 			info: "one level below max spell level"
+		},
+		{
+			name: "Spells learned from Scrolls",
+			bonus: {
+				data: [
+					"SPELLSKNOWN_WITCH_1+1",
+					"SPELLSKNOWN_WITCH_2+2",
+					"SPELLSKNOWN_WITCH_4+1",
+				],
+				type: ""
+			},
+			info: "Ill Omen (1), Delay Poison (2), Web (2), Secure Shelter (4)"
 		},
 		{
 			name: "Class Skills (Witch)",
@@ -156,7 +180,8 @@ characterSheet = {
 			name: "Alertness",
 			bonus: {
 				data: ["perception+2", "sense_motive+2"]
-			}
+			},
+			info: "from familiar"
 		},
 		{
 			name: "Witch class feats",
@@ -183,11 +208,8 @@ characterSheet = {
 		},
 		{name: "Improved Familiar (Feat 7)"},
 		{
-			name: "Vagabond Child (Regional Trait)",
-			bonus: {
-				data: ["cs_disable_device +3", "disable_device +1"],
-				type: "trait"
-			}
+			name: "Greater Spell Penetration (Feat 9)",
+			info: "+2 on caster level checks vs SR"
 		},
 		{name: "Disguise (Hex 1)"},
 		{name: "Misfortune (Hex 2)"},
@@ -195,6 +217,13 @@ characterSheet = {
 		{name: "Cackle (Hex 6)"},
 		{name: "Fortune (Hex 8)"},
 		{name: "Charm (Extra Hex)"},
+		{
+			name: "Vagabond Child (Regional Trait)",
+			bonus: {
+				data: ["cs_disable_device +3", "disable_device +1"],
+				type: "trait"
+			}
+		},
 		{
 			name: "World Traveler (Human Race Trait)",
 			bonus: {
@@ -220,10 +249,10 @@ characterSheet = {
 		}
 	],
 	spells: {
-		"Silent Image": {level: 1, source: "Witch Patron (Shadow)"},
-		Darkness: {level: 2, source: "Witch Patron (Shadow)"},
-		"Deeper Darkness": {level: 3, source: "Witch Patron (Shadow)"},
-		"Shadow Conjuration": {level: 4, source: "Witch Patron (Shadow)"},
+		"Animate Rope": {level: 1, source: "Witch Patron (Trickery)"},
+		"Mirror Image": {level: 2, source: "Witch Patron (Trickery)"},
+		"Major Image": {level: 3, source: "Witch Patron (Trickery)"},
+		"Phantasmal Killer": {level: 4, source: "Witch Patron (Trickery)"},
 		"Dancing Lights": {level: 0},
 		"Detect Magic": {level: 0},
 		"Detect Poison": {level: 0},
@@ -239,17 +268,20 @@ characterSheet = {
 		Command: {level: 1},
 		"Cure Light Wounds": {level: 1},
 		"Detect Secret Doors": {level: 1},
+		"Ill Omen": {level: 1},
 		"Mage Armor": {level: 1},
 		"Mask Dweomer": {level: 1},
 		"Obscuring Mist": {level: 1},
 		"Ray of Enfeeblement": {level: 1},
 		"Unseen Servant": {level: 1},
 		"Alter Self": {level: 2},
+		"Delay Poison": {level: 2},
 		"Detect Thoughts": {level: 2},
 		"False Life": {level: 2},
 		Glitterdust: {level: 2},
 		"Pox Postules": {level: 2},
 		"Vomit Swarm": {level: 2},
+		"Web": {level: 2},
 		"Dispel Magic": {level: 3},
 		Fly: {level: 3},
 		"Lightning Bolt": {level: 3},
@@ -258,8 +290,12 @@ characterSheet = {
 		Tongues: {level: 3},
 		"Black Tentacles": {level: 4},
 		Enervation: {level: 4},
+		Fear: {level: 4},
 		Scrying: {level: 4},
-		"Solid Fog": {level: 4}
+		"Secure Shelter": {level: 4},
+		"Solid Fog": {level: 4},
+		"Summon Monster V": {level: 5},
+		"Baleful Polymorph": {level: 5}
 	},
 	temporary: [],
 	attacks: {
